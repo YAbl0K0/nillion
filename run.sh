56f8e1d5db2cda43274d2acf3adfc0fb6f79cb87
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-29 22:12:26 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+sleep $(( RANDOM % 601 + 3000 ))
+docker run -v ./nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 accuse --rpc-endpoint "https://testnet-nillion-rpc.lavenderfive.com" --block-start 5127642
